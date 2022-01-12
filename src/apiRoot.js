6268f9b332fae5f6fc3a390886a3f8053bf9a50e
Tim <timothy.george.taylor@gmail.com>
2022-01-12 16:46:31 -0700

    testing different font

--- src/apiRoot.js
+++
@@ -1,4 +0,0 @@
-const DEV_URL = "http://localhost:3000/";
-const PROD_URL = "https://[APP-NAME].herokuapp.com/";
-const API_ROOT = process.env.NODE_ENV === "development" ? DEV_URL : PROD_URL;
-export default API_ROOT;
